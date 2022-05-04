5e2c3d9c933f891c5732e97f2aeffaf53fb60b90
Prathmesh112000 <87255237+Prathmesh112000@users.noreply.github.com>
2022-05-04 19:51:59 +0530

    works and service file updated

--- fit/src/App.js
+++ fit/src/App.js
@@ -3,12 +3,17 @@
 import Health from "./Health"
 import Features from './Features' 
 import Appintment from './Appintment';
+import Services from "./Services";
+import Works from "./Works"
 function App() {
   return (
     <div className="App">
      <Health/>
     <Features/>
     <Appintment/>
+    <Services/>
+
+    <Works/>
     </div>
   );
 }

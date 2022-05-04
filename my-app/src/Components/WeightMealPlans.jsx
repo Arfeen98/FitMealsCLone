6bd55841c159dbc2c32e5a1a1b28f0626040b91d
Arfeen98 <98758325+Arfeen98@users.noreply.github.com>
2022-05-04 11:56:16 +0530

    commit

--- my-app/src/Components/WeightMealPlans.jsx
+++ my-app/src/Components/WeightMealPlans.jsx
@@ -18,10 +18,14 @@ const WeightMealPlans = () => {
 
                   <p>Disclaimer: This isn’t going to be any type of a crash diet which will give you magical results; we’ll aim for 2-4kgs per month and all your meals will be designed by our nutritionists who focus not only on seeing results but also on ensuring that there’s no compromise on your mental and physical health.</p>
               </div>
-              <div id="left_cont2"></div>
+              <div id="left_cont2">
+                  <button className="btn">View Sample Menu</button>
+              </div>
           </div>
           <div id='right_cont'>
-              <div id="right_cont1"></div>
+              <div id="right_cont1">
+                  <h6>What our meals look like:</h6>
+              </div>
               <div id="right_cont2"></div>
           </div>
        </div>

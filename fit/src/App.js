985a61cf55076ccabecbaeef10f3ca1b564244ee
Prathmesh112000 <87255237+Prathmesh112000@users.noreply.github.com>
2022-05-04 19:05:14 +0530

    features added

--- fit/src/App.js
+++ fit/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Health from "./Health"
+import Features from './Features' 
 function App() {
   return (
     <div className="App">
      <Health/>
-    
+    <Features/>
     </div>
   );
 }

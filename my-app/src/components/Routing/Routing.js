2824d0b92cadda01ad8869f62c5af368e3b42f32
Bhushan Badgujar <88032071+BBHUSHAN98@users.noreply.github.com>
2022-05-08 00:09:48 +0530

    edit

--- my-app/src/components/Routing/Routing.js
+++ my-app/src/components/Routing/Routing.js
@@ -1,35 +1,6 @@
 import React from 'react'
-import { Route,Routes,BrowserRouter as Router } from 'react-router-dom'
-// import AMP from '../../AMP/AMP'
-// import KMP from '../../KMP/KMP'
-// import MMP from '../../MMP/MMP'
-// import OneWeekAthleticMealPlan from '../1WeekAthleticMealPlan/OneWeekAthleticMealPlan'
-// import OneWeekWeightLossPlan from '../1WeekWeightLossPlan/OneWeekWeightLossPlan'
-// import TwoWeekWeightLossPlan from '../2WeekWeightLossPlan/TwoWeekWeightLossPlan'
-// import ThreeWeekWeightLossPlan from '../3WeekWeightLossPlan/ThreeWeekWeightLossPlan'
-// // import TwoWeekAthleticMealPla from '../TwoWeekAthleticMealPla/TwoWeekAthleticMealPla'
-// import About_us from '../About_us/About_us'
-// import Blogs from '../Blogs/Blogs'
-// import Cart from '../Cart/Cart'
-// import Mycart from '../Cart/Mycart'
-// import Health from '../Health_Coaching/Health'
-// import Home from '../Home/Home'
-// import Nav from '../Layout/Nav'
-// import WLMP from '../WLMP/WLMP'
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import Login from '../Login/Login'
-// import Meals from '../Meals/Meals'
-// import OneWeekMMP from '../OneWeekMMP/OneWeekMMP'
-// import OneWeekMMP from '../OneWeekMMP/OneWeekMMP'
-// import Pnf from '../Pnf/Pnf'
-// import Products from '../Products/Products'
-// import ThreeWeekAthleticMealPlan from '../ThreeWeekAthleticMealPlan/ThreeWeeekAthleticMealPlan'
-// import TwoWeekAthleticMealPlan from '../TwoWeekAthleticMealPlan/TwoWeekAthleticMealPlan'
-// import TwoWeekMMP from '../TwoWeekMMP/TwoWeekMMP'
-// import ThreeWeekMMP from '../ThreeWeekMMP/ThreeWeekMMP'
-// import Keto_OneWeek from '../Keto_OneWeek/Keto_OneWeek'
-// import Keto_TwoWeek from '../Keto_TwoWeek/Keto_TwoWeek'
-// import Keto_FourWeek from '../Keto_FourWeek/Keto_FourWeek'
-// import Protected from './Protected'
 import Reg from '../Registration/Registration'
 
 
@@ -42,7 +13,7 @@ export default function
        
        <Route path="Login" element={<Login/>} />
        <Route path="registration" element={<Reg/>} />
-
+       
        </Routes>
    </Router>
   )
